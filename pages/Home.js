b6e964c9cc864275d843690ee47419b1990cb043
abuabdulrahaman <abuabdulrahaman21@gmail.com>
2022-03-29 15:08:52 +0000

    first commit

---
+++ pages/Home.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function Home() {
+  return (
+ <div>
+   <h1>welcome to my Homepage</h1> 
+    <div>
+    <p> in the reign of his great-grandmother Queen Victoria, 
+    he was named Albert at birth after his great-grandfather Albert,
+     Prince Consort, and was known as "Bertie" to his family and close friends. 
+     His father ascended the throne as King George V in 1910. As the second son of 
+     the king, Albert was not expected to inherit the throne. He spent his early life 
+     in the shadow of his elder brother, Prince Edward, the heir apparent. 
+     Albert attended naval college as a teenager and served in the Royal Navy and Royal Air Force during the First World War. In 1920, he was made Duke of York. He married Lady Elizabeth Bowes-Lyon in 1923, and they had two daughters, Elizabeth and Margaret. In the mid-1920s, he had speech therapy for a stammer, which he learned to manage to some degree. His elder brother ascended the throne as Edward VIII after their father died in 1936, but Edward abdicated later that year to marry the twice-divorced American socialite Wallis Simpson; Albert thereby became the third monarch of the House of Windsor, 
+    taking the regnal name George VI.</p>
+
+    <p> in the reign of his great-grandmother Queen Victoria, 
+    he was named Albert at birth after his great-grandfather Albert,
+     Prince Consort, and was known as "Bertie" to his family and close friends. 
+     His father ascended the throne as King George V in 1910. As the second son of 
+     the king, Albert was not expected to inherit the throne. He spent his early life 
+     in the shadow of his elder brother, Prince Edward, the heir apparent. 
+     Albert attended naval college as a teenager and served in the Royal Navy and Royal Air Force during the First World War. In 1920, he was made Duke of York. He married Lady Elizabeth Bowes-Lyon in 1923, and they had two daughters, Elizabeth and Margaret. In the mid-1920s, he had speech therapy for a stammer, which he learned to manage to some degree. His elder brother ascended the throne as Edward VIII after their father died in 1936, but Edward abdicated later that year to marry the twice-divorced American socialite Wallis Simpson; Albert thereby became the third monarch of the House of Windsor, 
+    taking the regnal name George VI.</p>
+
+    <p> in the reign of his great-grandmother Queen Victoria, 
+    he was named Albert at birth after his great-grandfather Albert,
+     Prince Consort, and was known as "Bertie" to his family and close friends. 
+     His father ascended the throne as King George V in 1910. As the second son of 
+     the king, Albert was not expected to inherit the throne. He spent his early life 
+     in the shadow of his elder brother, Prince Edward, the heir apparent. 
+     Albert attended naval college as a teenager and served in the Royal Navy and Royal Air Force during the First World War. In 1920, he was made Duke of York. He married Lady Elizabeth Bowes-Lyon in 1923, and they had two daughters, Elizabeth and Margaret. In the mid-1920s, he had speech therapy for a stammer, which he learned to manage to some degree. His elder brother ascended the throne as Edward VIII after their father died in 1936, but Edward abdicated later that year to marry the twice-divorced American socialite Wallis Simpson; Albert thereby became the third monarch of the House of Windsor, 
+    taking the regnal name George VI.</p>
+    
+    
+    
+    
+    
+    
+    </div>
+    
+    
+    
+</div>
+  )
+}
+
+export default withLayout(Home)
