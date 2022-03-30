8d64130d3205ccbec32f609c7c97452876072281
abuabdulrahaman <abuabdulrahaman21@gmail.com>
2022-03-30 14:34:57 +0000

    added route

---
+++ src/pages/Blog.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import withlayout from './withLayout'
+
+function Blog() {
+  return (
+    <div>
+    <h1>Welcome to my BlogPage</h1>
+    <div>
+    <p>The Home Depothttps://www.homedepot.com
+    Shop online for all your home improvement needs: appliances, bathroom decorating ideas, kitchen remodeling, patio furniture, power tools, bbq grills, ...
+    Credit Center · Truck & Tool Rental · Track Order · Special Buy of the Day
+     </p>
+     <p>The Home Depothttps://www.homedepot.com
+     Shop online for all your home improvement needs: appliances, bathroom decorating ideas, kitchen remodeling, patio furniture, power tools, bbq grills, ...
+     Credit Center · Truck & Tool Rental · Track Order · Special Buy of the Day
+     </p>
+     <p>The Home Depothttps://www.homedepot.com
+     Shop online for all your home improvement needs: appliances, bathroom decorating ideas, kitchen remodeling, patio furniture, power tools, bbq grills, ...
+     Credit Center · Truck & Tool Rental · Track Order · Special Buy of the Day
+     You might be tempted to choose distinctive fixtures for your bathroom, but this isn’t the most economical option. Jake Romano, who works for Ottawa-based plumbing company John The Plumber, said things like unique shower valves and faucets might look beautiful and interesting, but they can be pricey.
+
+“These fixtures don’t last forever, and they often need replacement parts like cartridges,” he said. “Replacement parts are not standardized. If you buy an extremely unique fixture from a relatively unknown brand, you’re going to have a difficult time finding replacement parts.”
+
+Opting for standard fixtures might not seem as fun, but you’ll likely be glad you did.
+
+“If you buy a fixture from a popular brand like Moen or Delta, you’ll likely be able to get a replacement part the same day you need it,” he said.
+
+You can’t put a price on the value of having a fully functioning bathroom at all times.n
+     </p>
+    </div>
+    </div>
+  )
+}
+
+export default withlayout(Blog)
---
+++ src/pages/withLayout.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Withlayout(Component) {
+    function Layout(){
+
+        return(
+            <div className='layout'>
+            <header className='appbar'>
+            <span>Logo</span>
+            <div>
+            <nav>
+            <span className='nav-link'><Link to="/"> Home </Link></span>
+            <span className='nav-link'><Link to="/about"> About </Link></span>
+            <span className='nav-link'><Link to="/blog"> Blog </Link></span>
+    
+            </nav>
+            </div>
+            </header>
+            <main className='main-component'>
+            <Component/>
+
+
+
+
+
+            </main>
+            <footer>&copy; 2022 Premest</footer>
+            </div>
+        )
+        
+    }
+  return Layout
+  
+}
+
+export default Withlayout
