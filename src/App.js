ed2cd8f489f72b4a39bf6bed09d52f433b972ed1
abuabdulrahaman <abuabdulrahaman21@gmail.com>
2022-04-01 17:40:30 +0000

    added some items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react'
 import Home from './pages/Home'
-import { Routes,Route, Navigate } from 'react-router-dom'
-import About from './pages/About'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Blog from './pages/Blog'
+import About from './pages/About'
+import BlogList from "./pages/Blog/blogs"
+import BlogDetails from "./pages/Blog/detail"
 
 function App() {
   return (
     <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/blog' element={<Blog/>}/>
-    <Route path='*' element={<Navigate to="/"/>}/>
-
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/blog" element={<Blog />}>
+        <Route index element={<BlogList />} />
+        <Route path=":blog" element={<BlogDetails />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Blog/detail.js
@@ -0,0 +1,30 @@
+import { useLocation } from "react-router-dom";
+import {useEffect, useState} from 'react'
+
+
+
+
+function BlogDetail(){
+    const location = useLocation()
+    const [post, setPost] = useState({});
+    useEffect(()=>{
+        setPost(location.state.post);
+    }, [location.state])
+return(
+    <div>
+        {post && (
+            <div>
+                <h2>{post.title}</h2>
+                <p>{post.extract}</p>
+                <img src={`/images/${post.image}`} alt="" height={350} width="100%"/>
+                <p>{post.body}</p>
+ </div>
+        )
+
+        }
+    </div>
+)
+
+
+}
+export default BlogDetail
